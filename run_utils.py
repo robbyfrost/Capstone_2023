0efddeb0b1ea0bc51c45a366a006f891a9f7036f
robbyfrost <robby@ou.edu>
2023-11-16 12:17:02 -0600

    case changes

--- run_utils.py
+++ run_utils.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------
 # Name: run_utils.py
-# Author: Robert M. Frost, Savannah J. Southward, and Colin K. Welty
+# Authors: Robert M. Frost, Savannah J. Southward, and Colin K. Welty
 # University of Oklahoma
 # Created: 09 November 2023
 # Purpose: Script to run functions from 
